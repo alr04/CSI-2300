e89eaf7db15a34c273d84ea667f2bc723177f1b9
alr04 <ashua2004@gmail.com>
2023-03-10 21:29:50 -0500

    updated

--- CSI-2300/Assignment 11/src/Count.java
+++ CSI-2300/Assignment 11/src/Count.java
@@ -1,5 +1,39 @@
-public class App {
+import java.io.*;
+import java.util.*;
+import java.util.Map.Entry;
+
+
+public class Count {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        String fileName = "words.txt";
+        
+        //  String[] words = {"hello", "world", "java"};
+        // Random random = new Random();
+        // int numOfLines = 100;
+        
+        // try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(fileName))) {
+        //     for (int i = 0; i < numOfLines; i++) {
+        //         bufferedWriter.write(words[random.nextInt(words.length)] + "\n");
+        //     }
+        // } 
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        String line; 
+        Map<String, Integer> counts = new TreeMap<>();
+        while ((line = bufferedReader.readLine()) != null) {
+            if (counts.containsKey(line)) {
+                counts.put(line, counts.get(line) + 1);
+            } else {
+                counts.put(line, 1);
+            }
+
+        }
+
+        String outFileName = "count.txt";
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(outFileName));
+        for (Entry<String, Integer> entry: counts.entrySet()) {
+            bufferedWriter.write(entry.getKey() + " " + entry.getValue());
+        }
+
     }
 }
